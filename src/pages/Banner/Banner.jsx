773d557b4c07623bd1cbdc9485f2512aea56da7d
sabrina islam <sabrinaislamrity@gmail.com>
2025-06-13 13:42:22 +0600

    seven

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -23,6 +23,12 @@ const Banner = () => {
             <h2>bb</h2>
             <h1>aaa</h1>
             <h2>bb</h2>
+            <h1>aaa</h1>
+            <h2>bb</h2>
+            <h1>aaa</h1>
+            <h2>bb</h2>
+            <h1>aaa</h1>
+            <h2>bb</h2>
         </div>
     );
 };
