84cb55ab105edcd0e2116143283482117fd344fa
sabrina islam <sabrinaislamrity@gmail.com>
2025-06-13 13:48:10 +0600

    sixteen

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -3,42 +3,7 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-          <h1>aaa</h1>
-            <h2>bb</h2><h1>aaa</h1>
-            <h2>bb</h2><h1>aaa</h1>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h2>bb</h2>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
-            <h1>aaa</h1>
-            <h2>bb</h2>
+            
         </div>
     );
 };
